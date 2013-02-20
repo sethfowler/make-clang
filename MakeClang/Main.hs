fcdd839e1926b8adf5bfd6efb05a659d8b92e5b6
User Name <Email Address>
2013-02-20 10:15:30 -0800

    Implemented addOrUpdateRecord

--- MakeClang/Main.hs
+++ MakeClang/Main.hs
@@ -5,5 +5,7 @@ import SourceDB
 main = do putStrLn "hello world"
           handle <- openDB
           putStrLn "got it opened!"
+          addOrUpdateRecord handle "/sprockets" "mksprocket /sprockets"
+          addOrUpdateRecord handle "/sprockets" "newmksprocket /sprockets"
           closeDB handle
           putStrLn "got it closed!"
--- MakeClang/SourceDB.hs
+++ MakeClang/SourceDB.hs
@@ -19,9 +19,11 @@ dbPath = SQLVarChar 2048
 sourceFileTable :: SQLTable
 sourceFileTable = Table "SourceFiles"
                   [
-                    Column "Path" dbPath [],
+                    Column "Path" dbPath [Unique],
                     Column "BuildCommand" dbString []
                   ] []
+sourceFileReplaceStatement =
+  "replace into SourceFiles (Path, BuildCommand) values (:path, :cmd)"
 
 schema = [sourceFileTable]
 
@@ -36,8 +38,13 @@ openDB = do handle <- openConnection dbFilename
 closeDB :: DBHandle -> IO ()
 closeDB = closeConnection
 
-addOrUpdateRecord :: IO ()
-addOrUpdateRecord = undefined
+addOrUpdateRecord :: DBHandle -> String -> String -> IO ()
+addOrUpdateRecord h path cmd = do result <- tryToInsert
+                                  case result of
+                                    Just e -> error e
+                                    _      -> return ()
+  where tryToInsert = execParamStatement_ h sourceFileReplaceStatement params
+        params = [(":path", Text path), (":cmd", Text cmd)]
 
 -- Checks that the database has the correct schema and sets it up if needed.
 ensureSchema :: DBHandle -> IO ()
